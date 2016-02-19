ef36cc485feb183de567f9523292a036d081b0d6
Sean O'Rourke <seorourke@coca-cola.com>
2016-02-19 14:19:42 +0000

    first commit

---
+++ ABIEntity/BallinaDatabaseEntity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using ABIEntity;
+
+namespace ABIEntity
+{
+    public class BallinaDatabaseEntity : IBallinaDatabaseEntity
+    {
+
+        private BallinaDBEntities objDB = new BallinaDBEntities();
+
+        public string UpdateBatchDataByEquipmentID(string EquipmentID)
+        {
+
+
+            //objDB.spGetOPCTags();
+
+            var simpleQuery =
+                from num in objDB.OPC_Tags
+                select num;
+
+            foreach (var item in simpleQuery)
+            {
+                //Debug.Print(item.RID.ToString());
+            }
+
+            var Results = objDB.spGetOPCTags1().ToList();
+
+
+            foreach(var items in Results)
+            {
+                Debug.Print(items.RID.ToString());
+            }
+ 
+
+            return "";
+        }
+
+    }
+}
---
+++ ABIEntity/IBallinaDatabaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABIEntity
+{
+    public interface IBallinaDatabaseEntity
+    {
+        //https://msdn.microsoft.com/nl-nl/library/87d83y5b.aspx
+
+        string UpdateBatchDataByEquipmentID(string EquipmentID);
+    }
+
+     
+}
